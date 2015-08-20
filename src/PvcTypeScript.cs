dfb951abde120ddaa5005e5aee15c1625f7eda73
Marcin Lewandowski <marcin@ais.pl>
2015-08-20 11:23:50 +0200

    monitoring tsc.exe exit status

--- src/PvcTypeScript.cs
+++ src/PvcTypeScript.cs
@@ -1,6 +1,7 @@
 using PvcCore;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -35,38 +36,64 @@ namespace PvcPlugins
 			Console.WriteLine("Using type script compiler: {0}", compilerPath);
 
 			var p = Path.GetTempFileName();
-            File.WriteAllText(p, string.Format("{0} {1}", this.compilerOptions, tsFilesString));
+	        try
+	        {
+		        File.WriteAllText(p, string.Format("{0} {1}", this.compilerOptions, tsFilesString));
 
-            var result = PvcUtil.StreamProcessExecution(compilerPath, Environment.CurrentDirectory, "@"+ p);
+		        var result = StreamProcessExecution(compilerPath, Environment.CurrentDirectory, "@" + p);
 
-            File.Delete(p);
+		        string tsOutputLine;
+		        var outStreamReader = new StreamReader(result.StandardOutput.BaseStream);
+		        while ((tsOutputLine = outStreamReader.ReadLine()) != null)
+		        {
+			        Console.WriteLine(tsOutputLine);
+		        }
 
-            string tsOutputLine;
-            var outStreamReader = new StreamReader(result.Item1);
-            while ((tsOutputLine = outStreamReader.ReadLine()) != null)
-            {
-                Console.WriteLine(tsOutputLine);
-            }
+		        var errorStreamReader = new StreamReader(result.StandardError.BaseStream);
+		        while ((tsOutputLine = errorStreamReader.ReadLine()) != null)
+		        {
+			        Console.WriteLine(tsOutputLine);
+		        }
 
-            var errorStreamReader = new StreamReader(result.Item2);
-            while ((tsOutputLine = errorStreamReader.ReadLine()) != null)
-            {
-                Console.WriteLine(tsOutputLine);
-            }
+		        result.WaitForExit();
+		        if (result.ExitCode != 0)
+		        {
+			        throw new Exception("Type script exited with non-zero status code: " + result.ExitCode);
+		        }
 
-            // TODO: Implement advanced compiler options that affect JS output.
-            //
-            // In the compiler options flag, if the user passes in either of the following options:
-            //      --out (combine JS output into single file option)
-            //      - "redirect JS output to directory" option
-            //      - "generate declaration files" option
-            //      - "generate source maps" options
-            // 
-            // Then we'll need to pick up the corresponding JS and .js.map files.
-            return tsFiles
-                .Select(f => f.Replace(".ts", ".js"))
-                .Where(f => File.Exists(f))
-                .Select(f => PvcUtil.PathToStream(f));
+		        // TODO: Implement advanced compiler options that affect JS output.
+		        //
+		        // In the compiler options flag, if the user passes in either of the following options:
+		        //      --out (combine JS output into single file option)
+		        //      - "redirect JS output to directory" option
+		        //      - "generate declaration files" option
+		        //      - "generate source maps" options
+		        // 
+		        // Then we'll need to pick up the corresponding JS and .js.map files.
+		        return tsFiles
+			        .Select(f => f.Replace(".ts", ".js"))
+			        .Where(f => File.Exists(f))
+			        .Select(f => PvcUtil.PathToStream(f));
+	        }
+	        finally
+	        {
+				File.Delete(p);
+			}
+         
         }
-    }
+
+		public static Process StreamProcessExecution(string processPath, string workingDirectory, params string[] args)
+		{
+			var startInfo = new ProcessStartInfo(processPath, string.Join(" ", args));
+			startInfo.UseShellExecute = false;
+			startInfo.CreateNoWindow = true;
+			startInfo.ErrorDialog = false;
+			startInfo.WorkingDirectory = workingDirectory;
+			startInfo.RedirectStandardError = true;
+			startInfo.RedirectStandardOutput = true;
+
+			var process = Process.Start(startInfo);
+			return process;
+		}
+	}
 }
